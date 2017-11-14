dbab1109ec4e064885aee61afb62dc75a5c6f6c6
nguyen_xuan_hai <nguyen_xuan_hai@realworld.jp>
2017-11-14 19:44:38 +0900

    int model

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
+use Illuminate\Notifications\Action;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
@@ -15,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'gender', 'phone_number', 'location', 'is_active'
     ];
 
     /**
@@ -26,4 +27,19 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function userPoint()
+    {
+        return $this->hasOne(UserPoint::class, 'user_id');
+    }
+
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
+
+    public function actions()
+    {
+        return $this->hasMany(Action::class, 'user_id');
+    }
 }
--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -14,9 +14,9 @@ class CreatePasswordResetsTable extends Migration
     public function up()
     {
         Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+            $table->string('email')->comment('パスワード再設定をリクエストしたいのユーザのメール');
+            $table->string('token')->comment('再設定トークン');
+            $table->timestamp('created_at')->nullable()->comment('パスワード再設定のリクエスト日時');
         });
     }
 
